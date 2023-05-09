8c0dbef4b3b3077d82118b43b63616744b598dea
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-05-09 18:13:56 +0300

    add propTypes and minimal css

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import css from 'components/FeedbackOptions/FeedbackOptions.module.css';
 
 export default class FeedbackOptions extends React.Component {
+  static propTypes = {
+    oprions: PropTypes.arrayOf(PropTypes.string),
+    onLeaveFeedback: PropTypes.func,
+  };
+
   render() {
     return (
-      <div className="FeedbackOptions">
+      <div className={css.container}>
         {this.props.options.map(label => (
           <button
+            className={css.button}
             key={label}
             type="button"
             onClick={() => this.props.onLeaveFeedback(label)}
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,9 +1,17 @@
+import PropTypes from 'prop-types';
+import css from 'components/Section/Section.module.css';
+
 const Section = ({ title, children }) => {
   return (
-    <section title={title}>
+    <section title={title} className={css.section}>
       <h2 className="FeedbackOptions__title">{title}</h2>
       {children}
     </section>
   );
 };
 export default Section;
+
+Section.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
